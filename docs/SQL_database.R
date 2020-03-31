c6d10bdd29f7fccdf1de0b56fdc4fa314bf84b8c
Carter Hanford <carter.hanford@slu.edu>
2020-03-31 10:26:02 -0500

    add SQL database R script

---
+++ docs/SQL_database.R
@@ -0,0 +1,29 @@
+# dependencies
+library(DBI)      # database management
+library(dplyr)    # data wrangling
+library(purrr)
+library(here)     # file path management
+library(janitor)  # data wrangling
+library(readxl)   # read excel files
+library(stringr)  # string wrangling
+library(tidyr)    # data wrangling
+library(tigris)   # download placenames
+library(sf)       # spatial tools
+
+# connect to database
+con <- dbConnect(RSQLite::SQLite(), "data/STL_CITY_COUNTY_Database.sqlite")
+
+# write population table
+copy_to(con, stl_population, "population",
+        temporary = FALSE,
+        overwrite = TRUE,
+        indexes = list(
+          "year", 
+          "GEOID",
+          "population"
+        )
+)      
+      
+# disconnect from database
+dbDisconnect(con)
+ rm(con)
